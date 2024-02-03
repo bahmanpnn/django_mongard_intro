8d1df64cdc6ab467d9f91e6d6af1c70ced43dfed
Bahman Pournazari <bahmanpn@gmail.com>
2024-02-03 14:22:41 +0330

    7-created todo model

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Todo(models.Model):
+    title=models.CharField(max_length=100,blank=False,null=False)
+    body=models.TextField(blank=True)
+    created_date=models.DateTimeField(auto_now_add=True)
